8a93144c5981586348d01a54553ebf84b92171b1
jin.jing <jingjin666@gmail.com>
2020-12-06 21:01:02 +0800

    修正NEU下的DCM矩阵

--- utils/dcm.m
+++ utils/dcm.m
@@ -9,38 +9,36 @@ Rz_NED = [cosYaw,sinYaw,0;
         -sinYaw,cosYaw,0;
         0,0,1];
 Ry_NED = [cosPitch,0,-sinPitch;
-    0,1,0;
-    sinPitch,0,cosPitch];
-
+        0,1,0;
+        sinPitch,0,cosPitch];
 Rx_NED = [1,0,0;
         0,cosRoll,sinRoll;
         0,-sinRoll,cosRoll];
     
-Rz_ENU = [cosYaw,sinYaw,0;
-        -sinYaw,cosYaw,0;
+Rz_ENU = [cosYaw,-sinYaw,0;
+        sinYaw,cosYaw,0;
         0,0,1];
-Ry_ENU = [cosPitch,0,-sinPitch;
-    0,1,0;
-    sinPitch,0,cosPitch];
-
+Ry_ENU = [cosRoll,0,sinRoll;
+        0,1,0;
+        -sinRoll,0,cosRoll];
 Rx_ENU = [1,0,0;
-        0,cosRoll,sinRoll;
-        0,-sinRoll,cosRoll];
+        0,cosPitch,-sinPitch;
+        0,sinPitch,cosPitch];
 
 %%ENU
-R_ENU_ZXY = Ry_ENU * Rx_ENU * Rz_ENU;    
+R_ENU_ZXY = Rz_ENU * Rx_ENU * Ry_ENU;    
 disp(R_ENU_ZXY);
     
 %%NED
 R_NED_ZYX_N2B = Rx_NED * Ry_NED * Rz_NED;
-disp(R_NED_ZYX_N2B);
+%disp(R_NED_ZYX_N2B);
 
 
 %%mag
 C_nb = Rx_NED * Ry_NED * eye(3);
-disp(C_nb);
+%disp(C_nb);
 
 syms Mx My Mz
 Mag_n = [Mx;My;Mz];
 Mag_b = C_nb * Mag_n;
-disp(Mag_b);
+%disp(Mag_b);
